4e708c32de020d0a5f373769b782c8fb75e07e02
Victor Pereira <vpereira@suse.de>
2014-08-08 15:36:52 +0200

    no need for a global temp directory

--- security-weekly.sh
+++ security-weekly.sh
@@ -18,33 +18,34 @@ set_tmpdir $0
 
 trap 'rm -rf $TMPDIR; exit 1' 0 1 2 3 13 15
 
+# push it to the functions.. no reason to be global
 OUT="$TMPDIR/security.out"
-TMP1="$TMPDIR/security.tmp1"
-TMP2="$TMPDIR/security.tmp2"
 
+# create SEC_DATA and SEC_VAR. directories used to store and persist data
 create_secdir
 
 # initialize rmp-md5, sbit, write, device and write-bin
 initialize_secfiles $SEC_DATA
 
-# get the ext2 and reiserfs mount points
+# get the fs mount points
 MNT=`/bin/mount | grep -E "^/dev/"  | cut -d' ' -f 3 | grep -v "/media" | xargs  echo "/dev/"`
 
+# set the mailer that will be used
 set_mailer
 
 # extended password check
 check_guessable_passwords "extended"
 
 # neverlogin check
-check_neverlogin $SEC_BIN $OUT
+check_neverlogin $SEC_BIN
 
-check_suid_gid $MNT $OUT
+check_suid_gid $MNT
 
-check_writable_executable $MNT $OUT
+check_writable_executable $MNT
 
-check world_writable $MNT $OUT
+check world_writable $MNT
 
-check_new_devices $MNT $OUT
+check_new_devices $MNT
 
 
 ####
